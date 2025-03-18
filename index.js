f76dd72f278e0550d1e8bf31b6ad52484cae471b
imajayupadhyay <ajayupadhyay030@gmail.com>
2025-03-18 17:53:20 +0530

    simple app for teting purpose

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const ip = require('ip');
+
+const app = express();
+
+
+app.get('/ping', (req, res) => {
+    
+    return res.json({
+        message: 'pong',
+        serverAddress: ip.address()
+    })
+});
+
+
+app.get('/heavy', (req, res) => {
+    console.log("looping")
+    for(let i = 0; i < 10000000000; i++) {}
+
+    return res.json({
+        message: 'heavy',
+        serverAddress: ip.address()
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
